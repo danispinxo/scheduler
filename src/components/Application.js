1854c44a10f8fb3eccbf0293f2fc87af146314ed
Dani Spinosa <danispinosa@Danis-MBP.ht.home>
2022-09-07 13:53:19 -0400

    making schedule viewable

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "components/Application.scss";
+import Appointment from "components/Appointment/";
 import DayList from "components/DayList";
 
 export default function Application(props) {
@@ -22,6 +23,55 @@ export default function Application(props) {
       spots: 0,
     },
   ];
+
+  const appointments = {
+    "1": {
+      id: 1,
+      time: "12pm",
+    },
+    "2": {
+      id: 2,
+      time: "1pm",
+      interview: {
+        student: "Lydia Miller-Jones",
+        interviewer:{
+          id: 3,
+          name: "Sylvia Palmer",
+          avatar: "https://i.imgur.com/LpaY82x.png",
+        }
+      }
+    },
+    "3": {
+      id: 3,
+      time: "2pm",
+    },
+    "4": {
+      id: 4,
+      time: "3pm",
+      interview: {
+        student: "Archie Andrews",
+        interviewer:{
+          id: 4,
+          name: "Cohana Roy",
+          avatar: "https://i.imgur.com/FK8V841.jpg",
+        }
+      }
+    },
+    "5": {
+      id: 5,
+      time: "4pm",
+    }
+  };
+
+  const appointmentList = Object.values(appointments).map(
+    (appointment) => {
+      return (
+        <Appointment 
+          key={appointment.id} 
+          {...appointment} 
+        />
+      )
+    });
 
   return (
     <main className="layout">
@@ -46,7 +96,8 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointmentList}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "components/Appointment/styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
-export default function Appointment(props) {
+export default function Appointment({ time, interview }) {
 
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={time} />
+      {interview ? <Show student={interview.student} interviewer={interview.interviewer}/> : <Empty />}
+    </article>
   );
 }
